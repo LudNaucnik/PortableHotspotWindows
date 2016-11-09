f9758adfc37b66c3a5ae7eedbc759441a770fc7e
Ljube <Ljube@LJUBE-PC>
2016-11-09 16:57:04 +0100

    display removed from preventing sleep

--- PortableHotspotWindows/MainForm.cs
+++ PortableHotspotWindows/MainForm.cs
@@ -19,7 +19,6 @@ namespace PortableHotspotWindows
         Boolean IsNetworkStarted;
         public const uint ES_CONTINUOUS = 0x80000000;
         public const uint ES_SYSTEM_REQUIRED = 0x00000001;
-        public const uint ES_DISPLAY_REQUIRED = 0x00000002;
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern uint SetThreadExecutionState([In] uint esFlags);
