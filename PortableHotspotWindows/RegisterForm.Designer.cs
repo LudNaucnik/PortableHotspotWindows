b055b218be33e5dc93072b30e40d3429529b6a76
Ljube <Ljube@LJUBE-PC>
2016-11-10 20:34:03 +0100

    Created Register Form and added controls

---
+++ PortableHotspotWindows/RegisterForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PortableHotspotWindows
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.RegisterTextBox = new System.Windows.Forms.TextBox();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key";
+            // 
+            // RegisterTextBox
+            // 
+            this.RegisterTextBox.Location = new System.Drawing.Point(80, 46);
+            this.RegisterTextBox.Name = "RegisterTextBox";
+            this.RegisterTextBox.Size = new System.Drawing.Size(204, 26);
+            this.RegisterTextBox.TabIndex = 1;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(303, 42);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(90, 30);
+            this.RegisterButton.TabIndex = 2;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 127);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.RegisterTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "RegisterForm";
+            this.Text = "Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox RegisterTextBox;
+        private System.Windows.Forms.Button RegisterButton;
+    }
+}
