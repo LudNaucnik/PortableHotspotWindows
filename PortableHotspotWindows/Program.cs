08137105c875aa12eb847ec98f637c606100e160
Ljube <Ljube@LJUBE-PC>
2016-12-18 18:07:18 +0100

    One instance of program

--- PortableHotspotWindows/Program.cs
+++ PortableHotspotWindows/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,12 +12,21 @@ namespace PortableHotspotWindows
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("Program already started", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
